efa40a8b31fe38159a34a024465073c176656c80
kaj_pesio <15047180+KajPe@users.noreply.github.com>
2018-10-15 23:12:58 +0300

    Teht 6.2

--- frontend/src/reducers/anecdoteReducer.js
+++ frontend/src/reducers/anecdoteReducer.js
@@ -20,18 +20,31 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (store = initialState, action) => {
-  if (action.type==='VOTE') {
-    const old = store.filter(a => a.id !==action.id)
-    const voted = store.find(a => a.id === action.id)
-
+  switch (action.type) {
+  case 'VOTE': {
+    const old = store.filter(a => a.id !==action.data.id)
+    const voted = store.find(a => a.id === action.data.id)
     return [...old, { ...voted, votes: voted.votes+1 } ]
   }
-  if (action.type === 'CREATE') {
-
-    return [...store, { content: action.content, id: getId(), votes:0 }]
+  case 'CREATE':
+    return [...store, { content: action.data.content, id: getId(), votes:0 }]
+  default:
+    return store
   }
+}
 
-  return store
+export const anecdoteCreation = (content) => {
+  return {
+    type: 'CREATE',
+    data: { content }
+  }
+}
+
+export const anecdoteVote = (id) => {
+  return {
+    type: 'VOTE',
+    data: { id }
+  }
 }
 
 export default reducer
